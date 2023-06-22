f0686887c1a1e624ac41ec407298011ef4ba4c79
Adam DuQuette <adam@roguegamgingstudio.com>
2023-06-22 15:06:03 -0700

    for way through prompt

--- 02-format-partitions-in-loop.sh
+++ 02-format-partitions-in-loop.sh
@@ -5,9 +5,9 @@ set -e
 echo "### 02 Format partitions in loop - STARTING"
 
 echo "1. Format the loop0p1 device (/boot)"
-sudo mkfs.ext4 /dev/loop0p1
+sudo mkfs.ext4 -F /dev/loop0p1
 
 echo "2. Format the loop0p2 device (/boot)"
-sudo mkfs.ext4 /dev/loop0p2
+sudo mkfs.ext4 -F /dev/loop0p2
 
 echo "### 02 Format partitions in loop - COMPLETE"
